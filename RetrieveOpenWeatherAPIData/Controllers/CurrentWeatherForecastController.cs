7889292c5ada32c3c5676b87b2b891555072ed99
benjmain <bdfulker2@hotmail.com>
2020-10-28 09:41:42 -0400

    Initial Commit

---
+++ RetrieveOpenWeatherAPIData/Controllers/CurrentWeatherForecastController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RetrieveOpenWeatherAPIData;
+
+namespace RetrieveOpenWeatherAPIData.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    [Route("api.openweather.org/data/2.5/weather?q={city name},{state code},{country code}&appid={/[controller]")]
+    [Consumes("application/json", "text/json")]
+    public class CurrentWeatherForecastController : ControllerBase
+    { 
+        private readonly ILogger<CurrentWeatherForecastController> _logger;
+
+        public CurrentWeatherForecastController(ILogger<CurrentWeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public ActionResult<CurrentWeatherForecast> Get()
+        {
+           
+        }
+    }
+}
---
+++ RetrieveOpenWeatherAPIData/CurrentWeatherForecast.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace RetrieveOpenWeatherAPIData
+{
+    public class CurrentWeatherForecast
+    {
+       
+    }
+}
