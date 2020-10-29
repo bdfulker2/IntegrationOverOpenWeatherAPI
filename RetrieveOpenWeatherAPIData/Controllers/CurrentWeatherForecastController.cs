5b5c1588469b1fddc58de7b721253362e1804241
benjmain <bdfulker2@hotmail.com>
2020-10-29 15:22:57 -0400

    Added comments

--- RetrieveOpenWeatherAPIData/Controllers/CurrentWeatherForecastController.cs
+++ RetrieveOpenWeatherAPIData/Controllers/CurrentWeatherForecastController.cs
@@ -1,21 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using RetrieveOpenWeatherAPIData;
 
 namespace RetrieveOpenWeatherAPIData.Controllers
 {
+    /// <summary>
+    /// Controller for making Http Requests to our API
+    /// </summary>
     [ApiController]
     [Route("api/[controller]")]
     [Consumes("application/json", "text/json")]
     public class CurrentWeatherForecastController : ControllerBase
     {
+        // Read only access to ICurrentWeatherService<CurrentWeatherForecastRoot>
         private readonly ICurrentWeatherForecastService<CurrentWeatherForecastRoot> _ICurrentWeatherService;
+        //Readonly access to ILogger
         private readonly ILogger _logger;
 
+        //Constructor with dependency injection for ICurrentWeatherForecastService and ILogger
         public CurrentWeatherForecastController(ICurrentWeatherForecastService<CurrentWeatherForecastRoot> currentWeatherService, ILogger<CurrentWeatherForecastController> logger)
         {
             _logger = logger;
@@ -25,11 +26,9 @@ namespace RetrieveOpenWeatherAPIData.Controllers
         [HttpGet]
         public ActionResult<CurrentWeatherForecastRoot> GetCurrentWeather(string city, string stateCode, string countryCode)
         {
-            //https://localhost:44353/api/CurrentWeatherForecast ?city=Naples&stateCode=FL&countryCode=US/
+            //https://localhost:44353/api/CurrentWeatherForecast?city=Naples&stateCode=FL&countryCode=US/
             _logger.LogDebug("HTTP request GetCurrentWeather() in CurrentWeatherForecastController");
             return _ICurrentWeatherService.GetCurrentWeather(city, stateCode, countryCode);
-            //return _ICurrentWeatherService.GetCurrentWeather("Naples", "FL", "US");
-
         }
     }
 }
