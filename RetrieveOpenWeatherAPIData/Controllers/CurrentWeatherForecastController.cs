96809bbbf5c10d8a4d9bb381999b44974b18bddc
benjmain <bdfulker2@hotmail.com>
2020-10-29 14:59:14 -0400

    Added CurrentWeatherForecastContoller logic

--- RetrieveOpenWeatherAPIData/Controllers/CurrentWeatherForecastController.cs
+++ RetrieveOpenWeatherAPIData/Controllers/CurrentWeatherForecastController.cs
@@ -8,5 +8,28 @@ using RetrieveOpenWeatherAPIData;
 
 namespace RetrieveOpenWeatherAPIData.Controllers
 {
-  
+    [ApiController]
+    [Route("api/[controller]")]
+    [Consumes("application/json", "text/json")]
+    public class CurrentWeatherForecastController : ControllerBase
+    {
+        private readonly ICurrentWeatherForecastService<CurrentWeatherForecastRoot> _ICurrentWeatherService;
+        private readonly ILogger _logger;
+
+        public CurrentWeatherForecastController(ICurrentWeatherForecastService<CurrentWeatherForecastRoot> currentWeatherService, ILogger<CurrentWeatherForecastController> logger)
+        {
+            _logger = logger;
+            _ICurrentWeatherService = currentWeatherService;
+        }
+
+        [HttpGet]
+        public ActionResult<CurrentWeatherForecastRoot> GetCurrentWeather(string city, string stateCode, string countryCode)
+        {
+            //https://localhost:44353/api/CurrentWeatherForecast ?city=Naples&stateCode=FL&countryCode=US/
+            _logger.LogDebug("HTTP request GetCurrentWeather() in CurrentWeatherForecastController");
+            return _ICurrentWeatherService.GetCurrentWeather(city, stateCode, countryCode);
+            //return _ICurrentWeatherService.GetCurrentWeather("Naples", "FL", "US");
+
+        }
+    }
 }
