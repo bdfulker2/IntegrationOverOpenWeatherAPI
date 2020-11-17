56b6059644b9d117a806b566ab4153488faae6e8
benjmain <bdfulker2@hotmail.com>
2020-11-17 12:10:56 -0500

    Added XML comments

--- RetrieveOpenWeatherAPIData/ICurrentWeatherForecastService.cs
+++ RetrieveOpenWeatherAPIData/ICurrentWeatherForecastService.cs
@@ -5,8 +5,18 @@ using System.Threading.Tasks;
 
 namespace RetrieveOpenWeatherAPIData
 {
+    /// <summary>
+    /// Interface for CurrentWeatherForecastService
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public interface ICurrentWeatherForecastService<T> where T : class
     {
+        /// <summary>
+        /// Interface for current weather forecast  
+        /// </summary>
+        /// <param name="city">string for weather forecast city</param>
+        /// <param name="stateCode">state code for US states only</param>
+        /// <param name="countryCode">country code</param>
         T GetCurrentWeather(string city, string stateCode, string countryCode);
     }
 }
